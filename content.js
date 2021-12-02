41137257983ccc9560893dbe20ae8c0f3a853219
VojtaSkarin <54547074+VojtaSkarin@users.noreply.github.com>
2021-12-02 11:12:49 +0100

    Firefox version

--- content.js
+++ content.js
@@ -1,5 +1,28 @@
-keys = ['mimo', 'otravne', 'prinosne', 'vtipne', 'zajimave', 'nemamNazor'];
+emojis = {
+	'mimo': 'KingPepega.png',
+	'otravne': 'Tarded.png',
+	'prinosne': 'WeSmart.png',
+	'vtipne': 'Kekw.png',
+	'zajimave': 'PagChomp.png',
+	'nemamNazor': 'Peeposhrug.png'
+};
 
+browser.storage.sync.set({'mimo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/How_to_use_icon.svg/1200px-How_to_use_icon.svg.png'});
+
+root = document.querySelector(':root');
+
+for ([key, value] of Object.entries(emojis)) {
+	root.style.setProperty('--' + key + 'Url', 'url(' + browser.runtime.getURL('/emojis/' + value) + ')');
+	root.style.setProperty('--' + key + 'DefaultUrl', 'url(' + browser.runtime.getURL('/emojis/' + value) + ')');
+}
+
+browser.storage.sync.get(emojis.keys).then(stored => {
+	for ([key, value] of Object.entries(stored)) {
+		root.style.setProperty('--' + key + 'Url', 'url(' + value + ')');
+	}
+});
+
+/*
 chrome.storage.sync.get(keys, items => {
 	root = document.querySelector(':root');
 	style = getComputedStyle(root);
@@ -8,3 +31,4 @@ chrome.storage.sync.get(keys, items => {
 		root.style.setProperty('--' + key + 'Url', 'url("' + value + '")');
 	}
 });
+*/
